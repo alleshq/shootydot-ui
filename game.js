a12ae8ff131ab7d5da46c12a15128dbf92316c54
Archie Baer <archie@abaer.dev>
2020-01-25 22:59:11 +0000

    Basic UI

--- game.js
+++ game.js
@@ -4,5 +4,5 @@ const serverUrl = "http://localhost:8081";
 //Socket.io
 const socket = io(serverUrl);
 socket.on("data", data => {
-    document.body.innerText = JSON.stringify(data);
+    document.body.innerText = JSON.stringify(data.players);
 });
